504353b8b7fd705ea3b2ef55b97f00481a1fb92a
Александр <a.semenov@abcmobile.com>
2022-05-06 13:08:09 +0300

    Поправил отображение картинок

--- src/components/UI/ButtonSmall/index.jsx
+++ src/components/UI/ButtonSmall/index.jsx
@@ -1,17 +1,30 @@
 import classNames from "classnames";
 import PropTypes from "prop-types";
 
+///images
+import Add from "../../../assets/icons/approve.svg";
+import Cancel from "../../../assets/icons/cancel.svg";
+
 // styles
 import "./style.scss";
 
 const ButtonSmall = ({ type, ...props }) => {
   let btnClass = classNames("btn-small", type);
+  let backgroundImg = {
+    backgroundImage: `url(${type === "cancel" ? Cancel : Add})`,
+  };
 
-  return <button className={btnClass} {...props}></button>;
+  return (
+    <button style={backgroundImg} className={btnClass} {...props}></button>
+  );
 };
 
 ButtonSmall.propTypes = {
   type: PropTypes.string,
+};
+
+ButtonSmall.defaultProps = {
+  type: "add",
 };
 
 export default ButtonSmall;
